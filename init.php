17cb8404ab674890f141b64c12c5f90c31d0eab5
GamerMine <maxime-sav@outlook.fr>
2020-03-21 15:58:09 +0100

    Minor Update

--- init.php
+++ init.php
@@ -1,8 +1,11 @@
 <?php
+  $fp = fopen("ip_addr.json", "w") or die("Unable to open file!");
   $session_id = $_GET["session-id"];
   if($session_id == null){
     echo json_encode(null);
   }else{
+    fwrite($fp, json_encode($session_id));
+    fclose($fp);
     echo json_encode($session_id);
   }
 ?>
