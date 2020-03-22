d2db3dfcbc0ae4d892436a85bf693e7245dcf0c1
GamerMine <maxime-sav@outlook.fr>
2020-03-22 16:07:05 +0100

    Minor Update -Separated readJSON function to a new file readJSON.js

--- js/initConnection.js
+++ js/initConnection.js
@@ -1,18 +1,3 @@
-function readJSON(file) {
-  var rawFile = new XMLHttpRequest();
-  var allText = null;
-  rawFile.open("GET", file, false);
-  rawFile.onreadystatechange = function (){
-      if(rawFile.readyState === 4){
-          if(rawFile.status === 200 || rawFile.status == 0){
-              allText = rawFile.responseText;
-          }
-      }
-  }
-  rawFile.send(null);
-  return JSON.parse(allText);
-}
-
 function triggerFondu() {
   var sessionInput = document.getElementById("sessionInput");
   var inputText = sessionInput.value;
---
+++ js/readJSON.js
@@ -0,0 +1,14 @@
+function readJSON(file) {
+  var rawFile = new XMLHttpRequest();
+  var allText = null;
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function (){
+      if(rawFile.readyState === 4){
+          if(rawFile.status === 200 || rawFile.status == 0){
+              allText = rawFile.responseText;
+          }
+      }
+  }
+  rawFile.send(null);
+  return JSON.parse(allText);
+}
