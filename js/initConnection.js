c1f680889d90f44ef46e676644d189c4630a05be
GamerMine <maxime-sav@outlook.fr>
2020-03-22 21:26:46 +0100

    Minor Update -Nothing to say

--- js/initConnection.js
+++ js/initConnection.js
@@ -1,48 +1,100 @@
 window.onload = eventsListeners
 
 
+function displayError() {
+  // Afficher un message (un div avec du text)
+  
+  var messageContainer = document.getElementById("message-container");
+  var messageDiv = document.createElement("div"); // Création de la div
+  var text = document.createTextNode("Session inexistante ou expirée. Vérifiez le code ou réouvrez une session."); // Ajout du texte (objet enfant)
+
+  messageDiv.appendChild(text);
+  messageDiv.classList.add("message-error"); // Ajout de la classe pour le style CSS
+  messageContainer.appendChild(messageDiv); // Ajour de l'élément à la page
+
+  // Animation d'affichage
+  var i = 0
+  var f = function() {
+    messageDiv.style.opacity = i;
+    i = i + 0.02;
+    if(i<1) {
+      setTimeout(f, 5);
+    }
+  };
+
+  f(); // Lancement de la boucle
+}
+
+
 function checkValidity() {
+  // Vérification de la validité du formulaire
+
   var sessionInput = document.getElementById("sessionInput");
-  var inputText = sessionInput.value;
-  var data = readJSON("sessions.json");
+  var inputText = sessionInput.value; // Récupération du texte saisie
+  var data = readJSON("sessions.json"); // Lecture des données enregistrées
 
   for(var key in data) {
     if (key.toLowerCase() == inputText.toLowerCase()) {
-      return true;
+      return true; // Si on trouve une correspondance entre ce qui est saisie et ce qui est enregistré alors on arrète
     }
   }
 
-  var sessionInput = document.getElementById("sessionInput");
-  var inputStyle = sessionInput.style;
+  // Sinon on continue et on fait les annimations d'erreurs
+
+  var inputStyle = document.getElementById("sessionInput").style;
   inputStyle.border = "3px solid red";
-  var errorMessage = document.getElementById("error-message");
-  var messageStyle = errorMessage.style
-  messageStyle.backgroundColor = "red";
-  messageStyle.visibility = "visible";
+  displayError()
   return false;
 }
 
 
 function submit(event) {
   event.preventDefault();
+
+  // Supprimer les anciens messages qui étaient affichés (avec animation)
+  Array.from(document.getElementsByClassName("message-error")).forEach(function(item) {
+    var i = 1
+    var f = function() {
+      item.style.opacity = i;
+      i = i - 0.02;
+      if(i>=0) {
+        setTimeout(f, 5);
+      }
+    };
+
+    f(); // Lancement de l'animation
+    item.parentNode.removeChild(item)
+  });
+
+  // Si le formulaire est correcte, alors faire l'animation puis rediriger
   if (checkValidity()) {
     setTimeout(tryConnection,1600);
-    fondu("fondu");
   }
 }
 
-
 function tryConnection() {
+  // Formulaire correcte, alors faire l'animation puis rediriger
   var sessionInput = document.getElementById("sessionInput");
   var inputText = sessionInput.value;
-  var ID = Math.random().toString(36).substr(2, 9);
+
+  var ID = Math.random().toString(36).substr(2, 9); // Générer un code aléatoire (id de joueur)
+
+  // Création et envois d'une requète vers un PHP pour enregister le joueur
   var xmlReq = new XMLHttpRequest();
   xmlReq.open("GET", "createPlayerID.php?playerid="+ID+"&session="+inputText, true);
   xmlReq.send();
-  window.location.assign("play.html?session="+inputText+"&playerid="+ID);
+  //TODO: Verifier que le serveur renvois bien "true"
+
+  fondu("fondu"); // Animation
+
+  // Tout est ok, on peut rediriger le client après l'animation
+  setTimeout(function() {
+    window.location.assign("play.html?session="+inputText+"&playerid="+ID);
+  }, 1600);
 }
 
 
 function eventsListeners() {
+  // Ajout du listener pour l'évènement de soumission du formulaire
   document.querySelector("form").addEventListener("submit", submit);
 }
